1e8aee9d88c7f4b6810c7a926a0b717604b86380
soufiane amabraa <amabraa.soufiane@gmail.com>
2023-04-21 21:36:42 +0000

    add win and lose cenditions

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,6 +15,9 @@ createApp({
             dTotal: 0,
             //score for the player
             score: 200,
+            //the bet
+            bet: 0,
+            resultMessage: '',
 
         }
     },
@@ -23,17 +26,54 @@ createApp({
         getRandomDice: function () {
             return Math.floor(Math.random() * (7 - 1) + 1)
         },
-        // the roll function to make the dice random numbers
-        roll() {
+        //get dices numbers
+        dices() {
             this.pDice1 = this.getRandomDice()
             this.pDice2 = this.getRandomDice()
             this.pDice3 = this.getRandomDice()
             this.dDice1 = this.getRandomDice()
             this.dDice2 = this.getRandomDice()
             this.dDice3 = this.getRandomDice()
-
+        },
+        //calculate total of all dice
+        dicesTotal() {
             this.pTotal = this.pDice1 + this.pDice2 + this.pDice3;
             this.dTotal = this.dDice1 + this.dDice2 + this.dDice3;
+        },
+        //the wining and losing cenditions 
+        result() {
+            //losing cenditions
+            if (this.pTotal > 12 || this.dTotal > this.pTotal && this.dTotal <= 12) {
+                this.score -= this.bet;
+                this.bet = 0;
+                this.resultMessage = 'sorry you lost your bet try agin'
+
+            }
+            //wining cenditions
+            else if (this.pTotal <= 12 && this.dTotal < this.pTotal || this.dTotal > 12 && this.pTotal <= 12) {
+                this.score += this.bet;
+                this.bet = 0;
+                this.resultMessage = 'congratulations you win play more'
+            }
+            //draw cenditions
+            else if (this.pTotal <= 12 && this.dTotal === this.pTotal) {
+                this.resultMessage = 'You Draw with the dealer try agin'
+            }
+            else if (this.pDice1 === 4 && this.pDice2 === 4 && this.pDice3 === 4 && this.dTotal < 12) {
+                this.score += this.bet * 2;
+                this.bet = 0;
+                this.resultMessage = 'congratulations you win double play more'
+            }
+            //gameover cenditions
+            if (this.score === 0) {
+                this.resultMessage = 'gameover you lost all your money'
+            }
+        },
+        // the roll function 
+        roll() {
+            this.dices();
+            this.dicesTotal();
+            this.result();
         },
 
 
@@ -64,5 +104,7 @@ createApp({
         }
     },
     computed: {
+
+
     }
 }).mount('#app')
